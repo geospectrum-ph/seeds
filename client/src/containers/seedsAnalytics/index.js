d1c1e3ffa1e6730b13e09639e0395cb9bf0e9153
Geospectrum Marketing Services - IT Department <52265967+geospectrum-ph@users.noreply.github.com>
2024-08-07 17:15:36 +0800

    Update index.js

--- client/src/containers/seedsAnalytics/index.js
+++ client/src/containers/seedsAnalytics/index.js
@@ -2,9 +2,9 @@
 import React from 'react';
 import axios from 'axios';
 import _without from "lodash/without";
-import { Button, Grid, Container, FormControl, InputLabel, MenuItem, Select, FormHelperText } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { Box, Button, Grid, Container, FormControl, InputLabel, MenuItem, Select, FormHelperText } from '@material-ui/core';
 import { DataGrid } from '@material-ui/data-grid';
+import { makeStyles } from '@material-ui/core/styles';
 
 import theme from '../../theme'
 import SeedsAnalyticsMap from './seedsAnalyticsMap/index'
@@ -137,6 +137,10 @@ export default function Analytics() {
     list.map((item) => ("id" in item ? fetchFeatures(item.id) : null));
   }, [list]);
 
+  React.useEffect(() => {
+    console.log(layers);
+  }, [layers]);
+
   const [layerSelected, setLayerSelected] = React.useState([]);
 
   const handleChange = (event) => {
@@ -149,7 +153,69 @@ export default function Analytics() {
     setLayerSelected([]);
   }
 
-  const handleAnalyzeLayers = (event) => {
+  // const columns = [
+  //   { field: 'id', headerName: 'ID', width: 90 },
+  //   {
+  //     field: 'firstName',
+  //     headerName: 'First name',
+  //     width: 150,
+  //     editable: true,
+  //   },
+  //   {
+  //     field: 'lastName',
+  //     headerName: 'Last name',
+  //     width: 150,
+  //     editable: true,
+  //   },
+  //   {
+  //     field: 'age',
+  //     headerName: 'Age',
+  //     type: 'number',
+  //     width: 110,
+  //     editable: true,
+  //   },
+  //   {
+  //     field: 'fullName',
+  //     headerName: 'Full name',
+  //     description: 'This column has a value getter and is not sortable.',
+  //     sortable: false,
+  //     width: 160,
+  //     // valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
+  //   },
+  // ];
+  
+  // const rows = [
+  //   { id: 1, lastName: 'Snow', firstName: 'Jon', age: 14 },
+  //   { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 31 },
+  //   { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 31 },
+  //   { id: 4, lastName: 'Stark', firstName: 'Arya', age: 11 },
+  //   { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+  //   { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+  //   { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+  //   { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+  //   { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  // ];
+
+  const [columns, setColumns] = React.useState([]);
+  const [rows, setRows] = React.useState([]);
+
+  const handleAnalyzeLayers = (id_array) => {
+    const shapefile = [];
+    
+    id_array.map(function (id) {
+      const object = layers.find(function (item) {
+        return (item.properties.mtd_id === id);
+      });
+
+      object ? shapefile.push(object) : null;
+    });
+
+    const shapefileActive = shapefile[0];
+
+    console.log(shapefileActive);
+
+    // setColumns([...Array(40)].map(e=>~~(Math.random()*40)));
+
     /* */
   }
 
@@ -204,9 +270,27 @@ export default function Analytics() {
             </Button>
           </Grid>
           <Grid item xs = { 12 } md = { 2 } container direction = "row" justifyContent = "flex-start" alignItems = "center">
-            <Button fullWidth className = { classes.analyzeButton } variant = "contained" onClick = { (event) => { handleAnalyzeLayers(event) } } style = { { height: "56px", marginTop: "-8px" } }>
+            <Button fullWidth className = { classes.analyzeButton } variant = "contained" onClick = { () => { handleAnalyzeLayers(layerSelected) } } style = { { height: "56px", marginTop: "-8px" } }>
               Analyze
             </Button>
+          </Grid>
+          <Grid item xs = { 12 } md = { 6 } container direction = "row" justifyContent = "flex-start" alignItems = "center">
+            <Box sx={{ height: 400, width: '100%' }}>
+              <DataGrid
+                rows={rows}
+                columns={columns}
+                initialState={{
+                  pagination: {
+                    paginationModel: {
+                      pageSize: 5,
+                    },
+                  },
+                }}
+                pageSizeOptions={[5]}
+                checkboxSelection
+                disableRowSelectionOnClick
+              />
+            </Box>
           </Grid>
         </Grid>
       </Container>
