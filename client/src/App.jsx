6e30eec3de67a388ae87d0e6a98124c10ef0e32d
Geospectrum Marketing Services - IT Department <52265967+geospectrum-ph@users.noreply.github.com>
2024-08-29 16:28:25 +0800

    commit-2024-08-29
    initial config with test page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+import logo from "/favicons/favicon.ico";
+
+export default function App() {
+  const [count, setCount]  =  React.useState(0);
+
+  window.onload = function () {
+    document
+      .getElementById("test-btn")
+      .addEventListener("click", function () {
+        setCount((count)  => (count + 1));
+      });
+  };
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href = "https://vitejs.dev" target = "_blank">
+          <img src = { logo }/>
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href = "https://react.dev" target = "_blank">
+          <img src = { logo }/>
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className = "card">
+        <button id = "test-btn">count is { count }</button>
+        <p>Edit <code>src/App.jsx</code> and save to test HMR</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className = "read-the-docs">Click on the Vite and React logos to learn more.</p>
     </>
   )
-}
-
-export default App
+};
