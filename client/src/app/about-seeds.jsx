b7632fd1bfc918e302fdd34a8f3b5aa51660922f
Geospectrum Marketing Services - IT Department <52265967+geospectrum-ph@users.noreply.github.com>
2024-09-02 11:03:34 +0800

    commit-2024-09-02
    modified main landing page routing; initial work on modularization

---
+++ client/src/app/about-seeds.jsx
@@ -0,0 +1,5 @@
+export default function AboutSEEDs() {
+  return (
+    <p>About SEEDs</p>
+  );
+}
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import App from "./app.jsx";
+import Home from "./app/home";
+import AboutSEEDs from "./app/about-seeds";
+import ContactUs from "./app/contact-us";
+import Error from "./app/error";
 
-window.onload = function () {
-  ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-      <App/>
-    </React.StrictMode>
+import sign_console from "./assets/signature";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path = "/" element = { <Home/> }/>
+        <Route path = "/home" element = { <Home/> }/>
+        <Route path = "/about-seeds" element = { <AboutSEEDs/> }/>
+        <Route path = "/contact-us" element = { <ContactUs/> }/>
+        <Route path = "*" element = { <Error/> }/> 
+      </Routes>
+    </BrowserRouter>
   );
-};
+}
+
+window.addEventListener("load", function () {
+  ReactDOM
+    .createRoot(document.getElementById("root"))
+    .render(
+      <React.StrictMode>
+        <App/>
+      </React.StrictMode>
+    );
+
+  sign_console();
+});
