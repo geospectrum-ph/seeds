0ffa736aac1c6ba0cc024d0706dd5d24af34f39c
Geospectrum Marketing Services - IT Department <52265967+geospectrum-ph@users.noreply.github.com>
2025-06-30 13:31:02 +0800

    build

--- client/src/index.js
+++ client/src/index.js
@@ -10,7 +10,7 @@ import App from "./containers";
 
 import "./index.css";
 
-import * as serviceWorker from "./serviceWorker";
+// import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   // <React.StrictMode>
@@ -33,4 +33,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change unregister() to register() below. Note that this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 
-serviceWorker.register();
+// serviceWorker.register();
